0beb636c6e396c16c366b3766fea3d5cbb00ada3
Davit Natenadze <davit.natenadze@gmail.com>
2024-06-21 21:03:54 +0200

    Initial Commit

---
+++ toDoCombine/toDoCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  toDoCombineApp.swift
+//  toDoCombine
+//
+//  Created by Davit Natenadze on 21.06.24.
+//
+
+import SwiftUI
+
+@main
+struct toDoCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
